0140aadff9f54bc5f3143cddc0f807909fca9520
MatthewHermans <84220181+MatthewHermans@users.noreply.github.com>
2024-06-06 12:37:29 +0200

    Initial Commit

---
+++ MacPlus/MacPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacPlusApp.swift
+//  MacPlus
+//
+//  Created by matthew hermans on 06/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MacPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
